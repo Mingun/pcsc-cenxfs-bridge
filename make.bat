eddecb6f7c19152bb064cb7201cc6cba4f29bfe7
Mingun <none@none>
2014-10-12 13:57:56 +0600

    Доработан make.bat для возможности компиляции из командной строки.

--- make.bat
+++ make.bat
@@ -1,3 +1,8 @@
 @echo off
 set "XFS_SDK=..\XFS SDK3.0\SDK"
-cl /LD /EHsc /I"%XFS_SDK%\INCLUDE" PCSCspi.cpp /link /LIBPATH:"%XFS_SDK%\LIB" /MACHINE:X86
+
+:clear
+del /F *.obj *.dll *.lib *.exp
+
+:build
+cl /LD /EHsc -D_WIN32_WINNT=0x0501 -DDEBUG /I"%XFS_SDK%\INCLUDE" /I"%BOOST_ROOT%" *.cpp /link /LIBPATH:"%XFS_SDK%\LIB" /LIBPATH:"%BOOST_ROOT%/stage/lib" /MACHINE:X86
