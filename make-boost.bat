3dc6fca2041171d4ef0e8417127582dd883ede52
Mingun <none@none>
2018-10-16 10:51:58 +0500

    В Readme добавлена инструкция для сборки буста, добавлен файл с необходимыми настройками сборщика буста

---
+++ make-boost.bat
@@ -0,0 +1,2 @@
+rem Параметры сборки библиотек буста, требуемые для проекта
+b2 --with-chrono --with-thread --with-date_time toolset=msvc link=static runtime-link=static threading=multi variant=release
